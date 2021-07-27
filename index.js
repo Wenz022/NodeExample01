9f1b4be946582c1d57640b6e3d6457583e12be54
Wenz022 <satech.hjwoo@gmail.com>
2021-07-27 15:20:37 +0900

    #템플릿 상속

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //const uuid4 = require("uuid4");
 //console.log(uuid4());
-/*
 
 const http = require("http");
 
 http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello Server");
+    response.write("Hello Server!!");
     response.end();
   })
   .listen(3000);
 
-*/
-
+/*
 const express = require("express");
 
 const app = express();
@@ -29,3 +27,5 @@ app.get("/fast", (req, res) => {
 app.listen(port, () => {
   console.log("Express listening on port", port);
 });
+
+*/
